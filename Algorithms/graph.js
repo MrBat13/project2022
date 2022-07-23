2cf5b4882ccfd99f803051e7901e46b44c76c9f6
MrBat13 <mrbat13@icloud.com>
2022-07-23 20:50:16 +0300

    vis-network

--- Algorithms/graph.js
+++ Algorithms/graph.js
@@ -34,3 +34,31 @@ class Graph {
         delete this.adjencyList[vertex];
     }
 }
+
+import { DataSet } from "vis-data/peer";
+import { Network } from "vis-network/peer";
+import "vis-network/styles/vis-network.css";
+
+const nodes = new DataSet([
+    {id: 1, label: "Node 1"},
+    {id: 2, label: "Node 2"},
+    {id: 3, label: "Node 3"},
+    {id: 4, label: "Node 4"},
+    {id: 5, label: "Node 5"}
+]);
+
+const edges = new DataSet([
+    {from: 1, to: 3},
+    {from: 1, to: 2},
+    {from: 2, to: 4},
+    {from: 2, to: 5}
+]);
+
+const container = document.getElementById('network');
+const data = {
+    nodes: nodes,
+    edges: edges
+};
+const options = {};
+
+const network = new Network(container, data, options);
