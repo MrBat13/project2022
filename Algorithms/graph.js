a22584fc115b35bd4e74649730f51cfa29f5e4f5
MrBat13 <mrbat13@icloud.com>
2022-07-24 01:38:01 +0300

    change to cytoscape

--- Algorithms/graph.js
+++ Algorithms/graph.js
@@ -1,64 +1,42 @@
-'use strict';
-class Graph {
-    constructor() {
-        this.adjencyList = {};
-    }
+const cy = cytoscape({
+    container: document.getElementById('cy'),
 
-    addVertex(vertex) {
-        if (!this.adjencyList[vertex]) {
-            this.adjencyList[vertex] = [];
+    elements: [
+        {
+            data: {id: 'a'}
+        },
+        {
+            data: {id: 'b'}
+        },
+        {
+            data: {id: 'ab', source: 'a', target: 'b'}
         }
-    }
+    ],
+    style: [ // the stylesheet for the graph
+        {
+            selector: 'node',
+            style: {
+                'background-color': '#000',
+                'label': 'data(id)'
+            }
+        },
 
-    addEdge(source, destination) {
-        if (!this.adjencyList[source]) {
-            this.addVertex(source);
+        {
+            selector: 'edge',
+            style: {
+                'width': 3,
+                'line-color': '#000',
+                'target-arrow-color': '#000',
+                'target-arrow-shape': 'triangle',
+                'curve-style': 'bezier'
+            }
         }
-        if (!this.adjencyList[destination]) {
-            this.addVertex(destination);
-        }
-        this.adjencyList[source].push(destination);
-        this.adjencyList[destination].push(source);
-    }
+    ],
 
-    removeEdge(source, destination) {
-        this.adjencyList[source] = this.adjencyList[source].filter(vertex => vertex !== destination);
-        this.adjencyList[destination] = this.adjencyList[destination].filter(vertex => vertex !== source);
-    }
+    layout: {
+        name: 'grid',
+        rows: 1
+    },
 
-    removeVertex(vertex){
-        while (this.adjencyList[vertex]) {
-            const adjacentVertex = this.adjencyList[vertex].pop();
-            this.removeEdge(vertex, adjacentVertex);
-        }
-        delete this.adjencyList[vertex];
-    }
-}
-
-import { DataSet } from "vis-data/peer";
-import { Network } from "vis-network/peer";
-import "vis-network/styles/vis-network.css";
-
-const nodes = new DataSet([
-    {id: 1, label: "Node 1"},
-    {id: 2, label: "Node 2"},
-    {id: 3, label: "Node 3"},
-    {id: 4, label: "Node 4"},
-    {id: 5, label: "Node 5"}
-]);
-
-const edges = new DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
-]);
-
-const container = document.getElementById('network');
-const data = {
-    nodes: nodes,
-    edges: edges
-};
-const options = {};
-
-const network = new Network(container, data, options);
+    userZoomingEnabled: true
+});
