6516036d53d19d95fcd824c974879b112ccc8604
MrBat13 <mrbat13@icloud.com>
2022-08-12 23:48:25 +0300

    Matrix

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const elMatrix = document.querySelector('#divMatrix')
 const elGraph = document.querySelector('#canvasGraph')
 
 
-
 const createMatrixModel = () => {
     // TODO заменить на обход DOM дерева
-    return [ [0, 1, 1], [1, 1, 1], [0, 1, 0]]
+    const count = parseInt(elNodesCount.value)
+    const model = [];
+    for (let x = 0; x < count; x++){
+        model[x] = [];
+        for (let y = 0; y < count; y++){
+            let node = document.getElementById(x +''+ y);
+            model[x][y] = node.value;
+        }
+    }
+    model.forEach(function (entry){
+        console.table(entry);
+    });
+    return model;
 }
 
 const convertMatrixModelToGraph = (model) => {
@@ -17,32 +28,33 @@ const convertMatrixModelToGraph = (model) => {
 
 const renderGraph = (model) => {
     // отрисовка
-    const ctx = elGraph.getContext('2d')
+    const ctx = elGraph.getContext('2d');
 
-    ctx.beginPath();
+/*    ctx.beginPath();
     ctx.ellipse(100, 100, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.stroke();*/
 }
 
 
 changeCell = () => {
-    const matrixModel = createMatrixModel()
-    const graph = convertMatrixModelToGraph(matrixModel)
-    renderGraph(graph)
+    const matrixModel = createMatrixModel();
+    const graph = convertMatrixModelToGraph(matrixModel);
+    renderGraph(graph);
 }
 
 
 elApply.addEventListener('click', () => {
-    const count = parseInt(elNodesCount.value)
+    const count = parseInt(elNodesCount.value) // <2 должен выводить ошибку; =>2  +1 к числу
     elMatrix.innerHTML = ''
-    for (let y = 0; y < count; y++) {
+    for (let x = 0; x < count; x++) {
         const elRow = document.createElement('div')
         elMatrix.appendChild(elRow);
-        for (let x = 0; x < count; x++) {
-            const elCell = document.createElement('input')
-            elCell.type = 'text'
-            elCell.addEventListener('change', changeCell)
-            elRow.appendChild(elCell)
+        for (let y = 0; y < count; y++) {
+            const elCell = document.createElement('input');
+            elCell.type = 'text';
+            elCell.id = x + '' + y;
+            elCell.addEventListener('change', changeCell);
+            elRow.appendChild(elCell);
         }
     }
 })
