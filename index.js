97b5912bec04d7c6dab950a4c740e13e79a98c70
Michael <mrbat13@icloud.com>
2022-08-19 20:13:00 +0300

    MatrixToGraph

--- index.js
+++ index.js
@@ -5,65 +5,66 @@ const elGraph = document.querySelector('#canvasGraph')
 
 
 const createMatrixModel = () => {
-    // TODO заменить на обход DOM дерева
     const count = parseInt(elNodesCount.value);
     const model = [];
     for (let x = 0; x < count; x++){
         model[x] = [];
         for (let y = 0; y < count; y++){
-            let node = document.getElementById(x +'_'+ y);
+            let node = document.getElementById(x + '_' + y);
             model[x][y] = node.value;
         }
     }
     return model;
 }
-
+//я хотел сделать так, если в матрице элемент равен 0, значит нет ячейки => нет соседей, нет веса. но мой код
 const convertMatrixModelToGraph = (model) => {
-    // TODO преобразовать матрицу в дерево
-/*    const count = parseInt(elNodesCount.value);
-    for (let x = 0; x < count; x++){
-        for (let y = 0; y < count; y++){
-
-            //model[x][y] = '';
+    const graph = [];
+    const count = parseInt(elNodesCount.value);
+    for (let x = 0; x < count; x++) {
+        graph[x] = [];
+        for (let y = 0; y < count; y++) {
+            if (model[x][y] !== 0) {
+                graph[x][y] = {to: `Node${x}`, weight: model[x][y]};
+            }
+            if (model[x][y] === '0' || model[x][y] === ''){
+                graph[x][y] = ''
+            }
         }
-    }*/
+    }
+    console.table(graph);
+    return graph;
 
-
-
-    const node1 = { name : 'Узел 1', links : [] }
-    const node2 = { name : 'Узел 2', links : [] }
-    const node3 = { name : 'Узел 3', links : [] }
-    node1.links = [{ link : node2, distance : 800}, {link : node3, distance: 500}]
-    node2.links = [{ link : node3, distance : 400}]
-    return [node1, node2, node3]
 }
 
-const renderGraph = (model) => {
+const renderGraph = (graph) => {
     // отрисовка
     const ctx = elGraph.getContext('2d');
-
-    const cnt = model.length
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.clearRect(0, 0, elGraph.width, elGraph.height)
+    const cnt = graph.length
     const step = 360.0 / cnt * Math.PI / 180
 
-    // TODO - получит реальные размеры области вместо констант
-    const w = 800;
-    const h = 600;
-    const w2 = w * 0.5
-    const h2 = h * 0.5
-    const r = 200
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    const w2 = w / 2;
+    const h2 = h / 2;
+    const r = 100;
 
     let i = 0
-    for (let node of model) {
+    for (let node of graph) {
 
         const x = Math.cos(i * step) * r
         const y = Math.sin(i * step) * r
 
         ctx.beginPath();
         ctx.ellipse(w2 + x, h2 + y, 50, 50, 0, 0, 2 * Math.PI);
-
+        ctx.fillText(`Node${i}`, w2 + x, h2 + y);
         ctx.stroke();
         i++
     }
+
+
 }
 
 
@@ -76,23 +77,24 @@ changeCell = () => {
 
 elApply.addEventListener('click', () => {
     let count = parseInt(elNodesCount.value)
-    if (count<2){
+    if (count < 2) {
         alert('Enter at least 2 nodes');
-    } else count +=1;
+
+    }
 
     elMatrix.innerHTML = ''
-    for (let x = 1; x < count; x++) {
+    for (let x = 0; x < count; x++) {
         const elRow = document.createElement('div')
         elMatrix.appendChild(elRow);
         // TODO - добавить span
-        elRow.insertAdjacentHTML('beforeend',`<span> Node ${x}</span>`)
+        elRow.insertAdjacentHTML('beforeend', `<span> Node ${x}</span>`)
 
-        for (let y = 1; y < count; y++) {
+        for (let y = 0; y < count; y++) {
             const elCell = document.createElement('input');
             elCell.type = 'text';
             elCell.id = x + '_' + y;
-            if (x === y){
-                elCell.value = '0';
+            if (x === y) {
+                elCell.value = `0`;
             }
             elCell.addEventListener('change', changeCell);
             elRow.appendChild(elCell);
