cb42c696181a34044543b14fc43ac1d3133bb327
MrBat13 <mrbat13@icloud.com>
2022-09-14 15:29:59 +0300

    cytoscape

--- Algorithms/Dijkstra.js
+++ Algorithms/Dijkstra.js
@@ -2,7 +2,77 @@ import {numberToLetter} from "../index.js";
 /*
 array[x] = [{...},{from: '', to: '', weight: ''}, {...}]
  */
+const elInput = document.getElementById('inputBox')
+const V = elInput.value;
 
-export function dijkstra(array, startNode, endNode ) {
+function randomMatrix() {
+    // A B C D E F G H I
+    // 0 1 2 3 4 5 6 7 8
 
+    let array = new Array(V);
+    for (let x = 0; x < V; x++) {
+        array[x] = new Array(V);
+        for (let y = 0; y <= x; y++) {
+            if (x === y) {
+                array[x][y] = 0;
+            } else {
+                array[x][y] = {from: numberToLetter(x), to: numberToLetter(y), weight: Math.floor(Math.random() * 21)}
+                array[y][x] = array[x][y];
+            }
+        }
+    }
+    console.log(array)
+    return array;
+}
+
+
+function minDistance(dist,sptSet) {
+    // Initialize min value
+    let min = Infinity;
+    let min_index = -1;
+
+    for (let v = 0; v < V; v++) {
+        if (sptSet[v] === false && dist[v] <= min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+function printSolution(dist, src, dest) {
+    console.log(`From ${src} To ${dest}`);
+    let sum = 0
+    for (let i = 0; i < V; i++) {
+        sum += dist[i];
+    }
+    console.log(sum);
+}
+
+export function dijkstra(src, dest) {
+    let graph = randomMatrix();
+    let dist = new Array(V);
+    let sptSet = new Array(V);
+
+    for (let i = 0; i < V; i++) {
+        dist[i] = Infinity;
+        sptSet[i] = false;
+    }
+
+    dist[src] = 0;
+
+    // Find the shortest path for all vertices
+    for (let count = 0; count < V; count++) {
+        let u = minDistance(dist, sptSet);
+        sptSet[u] = true;
+
+        for (let v = 0; v < V; v++) {
+            if (!sptSet[v] && graph[u][v] !== 0 &&
+                dist[u] !== Infinity && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+    // Print the constructed distance array
+    //printSolution(dist, src, dest);
 }
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import {dijkstra} from "./Algorithms/Dijkstra.js";
+import {render} from "./Algorithms/visualization.js";
 
 const elInput = document.getElementById('inputBox')
 const elCanvasTrigger = document.getElementById('showCanvas')
-const elMatrixTrigger = document.getElementById('showMatrix')
-const elCanvas = document.getElementById('canvas')
-const elMatrix = document.getElementById('divMatrix')
 const elAlgorithmTrigger = document.getElementById('showAlgorithm')
 const elInputStart = document.getElementById('inputStart')
 const elInputEnd= document.getElementById('inputEnd')
@@ -45,76 +43,14 @@ function createArray() {
     return array;
 }
 
-function renderCanvas(array) {
-    const ctx = elCanvas.getContext('2d');
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight - 200;
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    const radius = 15;
-    const nodes = [];
-    //Отрисовка узла
-    for (let x = 0; x < array.length; x++) {
-        let X = Math.random() * (ctx.canvas.width);
-        let Y = Math.random() * (ctx.canvas.height);
-        nodes[x] = {posX: `${X}`, posY: `${Y}`};
-        ctx.beginPath();
-        ctx.arc(X, Y, radius, 0, Math.PI * 2, false);
-        ctx.fillText(`${x}`, X, Y);
-        ctx.stroke();
-    }
-    //Отрисовка ребра
-    for (let x = 0; x < array.length; x++) {
-        for (let y = 0; y < array.length; y++) {
-            if (array[x][y] !== '' && array[x][y].weight !== '0') {
-                ctx.beginPath()
-                ctx.moveTo(nodes[y].posX, nodes[y].posY)
-                ctx.lineTo(nodes[x].posX, nodes[x].posY)
-                ctx.stroke();
-            }
-        }
-    }
+function dijkstraTrigger(){
+    dijkstra(elInputStart.value, elInputEnd.value);
 
 }
 
-function renderMatrix() {
-    let count = parseInt(elInput.value)
-    if (count < 2) {
-        alert('Enter at least 2 nodes');
-    }
-    if (count >= 2) {
-        elMatrix.innerHTML = ''
-        for (let x = 0; x < count; x++) {
-            const elRow = document.createElement('div')
-            elMatrix.appendChild(elRow);
-            // TODO - добавить span
-            elRow.insertAdjacentHTML('beforeend', `<span> ${numberToLetter(x)} </span>`)
-            // y <= x вывод "лестницей"
-            for (let y = 0; y < count; y++) {
-                const elCell = document.createElement('input');
-                elCell.type = 'text';
-                elCell.id = x + '_' + y;
-                if (x === y) {
-                    elCell.value = `0`;
-                }
-                //elCell.addEventListener('change', changeCell);
-                elRow.appendChild(elCell);
-            }
-        }
-    }
+function canvasTrigger(){
+    render();
 }
 
-const dijkstraTrigger = () => {
-    let array = createArray();
-    let result = dijkstra(array, elInputStart, elInputEnd);
-}
-
-const canvasTrigger = () => {
-    let array = createArray();
-    renderCanvas(array);
-}
-
-elInput.addEventListener('change', renderMatrix)
-elMatrixTrigger.addEventListener('click', renderMatrix)
 elCanvasTrigger.addEventListener('click', canvasTrigger)
 elAlgorithmTrigger.addEventListener('click', dijkstraTrigger)
-
